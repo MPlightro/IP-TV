feebc0aa951de10539e9cead4f5baf68d7ec8c55
MPlightro <118073034+MPlightro@users.noreply.github.com>
2025-09-27 12:57:15 +0300

    xstream fix?

--- api/player_api.js
+++ api/player_api.js
@@ -1,6 +1,3 @@
-// api/player_api.js
-// IPTV Smarters compatible Player API (Xtream Codes style)
-
 const https = require("https");
 
 function fetchJson(url) {
@@ -31,68 +28,60 @@ function parseUsers() {
 
 module.exports = async (req, res) => {
   const users = parseUsers();
-  const { username, password } = req.query;
+  const { username, password, action } = req.query;
 
   if (!username || !password || users[username] !== password) {
-    return res.json({
-      user_info: {
-        auth: 0,
-        status: "Invalid",
-      },
-      server_info: {},
-    });
+    return res.json({ user_info: { auth: 0, status: "Invalid" }, server_info: {} });
   }
 
   const API_KEY = process.env.GOOGLE_API_KEY;
   const FOLDER_ID = process.env.FOLDER_ID;
 
-  try {
-    const url = `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents&key=${API_KEY}&fields=files(id,name)`;
-    const data = await fetchJson(url);
-    const files = data.files || [];
-
-    const vod = files.map((f, i) => ({
-      num: i + 1,
-      name: f.name,
-      stream_type: "movie",
-      stream_id: f.id,
-      stream_icon: "", // you can add poster URLs later
-      added: Date.now().toString(),
-      category_id: 1,
-      container_extension: "mp4",
-      direct_source: `https://drive.google.com/uc?id=${f.id}&export=download`,
-    }));
-
-    const response = {
+  // --- Case 1: No action, just user + server info ---
+  if (!action) {
+    return res.json({
       user_info: {
         username,
         password,
-        message: "Active",
-        auth: 1,
         status: "Active",
+        auth: 1
       },
       server_info: {
         url: "ip-tv-psi.vercel.app",
-        port: 443,
-        https_port: 443,
+        port: "80",
+        https_port: "443",
         server_protocol: "https",
-        timezone: "UTC",
-      },
-      categories: [
-        {
-          category_id: 1,
-          category_name: "Movies",
-          parent_id: 0,
-        },
-      ],
-      available_channels: vod.length,
-      vod,
-    };
-
-    res.setHeader("Content-Type", "application/json");
-    res.json(response);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Error generating player API" });
+        timezone: "UTC"
+      }
+    });
   }
+
+  // --- Case 2: Get VOD streams ---
+  if (action === "get_vod_streams") {
+    try {
+      const url = `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents&key=${API_KEY}&fields=files(id,name)`;
+      const data = await fetchJson(url);
+      const files = data.files || [];
+
+      const vod = files.map((f, i) => ({
+        num: i + 1,
+        name: f.name,
+        stream_type: "movie",
+        stream_id: f.id,
+        stream_icon: "",
+        added: Date.now().toString(),
+        category_id: 1,
+        container_extension: "mp4",
+        direct_source: `https://drive.google.com/uc?id=${f.id}&export=download`
+      }));
+
+      return res.json(vod);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error fetching Google Drive files" });
+    }
+  }
+
+  // --- Other actions can be added later ---
+  return res.json({ error: "Action not supported" });
 };
